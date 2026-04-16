3ea9ce4ee1ffa46dd1d03c365afea87dddd3ee47
Júlia Montagnini <125402201+jumontag@users.noreply.github.com>
2026-04-16 14:01:26 -0300

    automatização de testes + plot dos histogramas

--- Estruturas de Dados II/Trabalho prático I/codigo_fonte.py
+++ Estruturas de Dados II/Trabalho prático I/codigo_fonte.py
@@ -3,6 +3,7 @@ import math
 import statistics
 import time
 import csv
+import matplotlib.pyplot as plt
 
 def hash_soma_palavra_toda (palavra, tamanho_tabela):
     soma = 0
@@ -62,46 +63,58 @@ def busca_sequencial_na_lista(chave, lista_palavras):
     tempo_total = tempo_final - tempo_inicial
     return passos, tempo_total, None
 
+textos = ["tale.txt", "quincas_borba.txt"]
+M = [97, 100, 997]
+hashs = [hash_soma_palavra_toda, hash_soma_ponderada, hash_polinomial, hash_simples]
+nomes_funcoes_hash = ["hash_soma_palavra_toda", "hash_soma_ponderada", "hash_polinomial", "hash_simples"]
+
 # criação do csv que armazenará os resultados
 with open('resultados.csv', 'w', newline='', encoding='utf-8') as csvfile:
                 writer = csv.writer(csvfile, delimiter=',')
                 writer.writerow(['texto', 'M', 'hash_name', 'n', 'alpha', 'max_bucket', 'avg_bucket', 'total_comp_success', 'avg_comp_success', 'total_comp_fail', 'avg_comp_fail'])
 
-okcsv = input("Deseja gerar arquivos .csv com dados dos histogramas? (sim/nao) ")
-okterminal = input("Deseja imprimir no terminal os histogramas? (sim/nao) ")
+# conjunto para os testes de falha (palavras que não existem nos textos)
+with open("nomes_filtrados.txt", "r", encoding="utf-8") as arquivo:
+    conjunto_falha = arquivo.read()
+    minusculo = conjunto_falha.lower()
+    palavras_f = re.findall(r'\b[a-zA-ZÀ-ÿ]+\b', minusculo)
 
-# automatizando os testes
-textos = ["tale.txt", "quincas_borba.txt"]
-M = [97, 100, 997]
-hashs = [hash_soma_palavra_toda, hash_soma_ponderada, hash_polinomial, hash_simples]
-nomes_funcoes_hash = ["hash_soma_palavra_toda", "hash_soma_ponderada", "hash_polinomial", "hash_simples"]
+lista_falha = []
+for i in range (len(palavras_f)):
+        if (palavras_f[i] not in lista_falha):
+            lista_falha.append(palavras_f[i])
+
+okimagens = input("Deseja visualizar os histogramas em uma nova janela? (sim/nao) ")
+okterminal = input("Deseja imprimir no terminal os histogramas? (sim/nao) ")
 num_experimento = 0
-for j in range (2):
-    for k in range(3):
-        for l in range (4):
+
+for j in range (2): # varia os textos
+    # tratamento de texto
+    arquivo_de_texto = textos[j]
+    with open(arquivo_de_texto, "r", encoding = "utf-8") as arquivo:
+        texto = arquivo.read()
+        texto_minusculo = texto.lower()
+        palavras = re.findall(r'\b[a-zA-ZÀ-ÿ]+\b', texto_minusculo)
+    
+    # removendo palavras repetidas e já armazenando na estrutura sequencial
+    lista_sequencial = []
+    for i in range (len(palavras)):
+        if (palavras[i] not in lista_sequencial):
+            lista_sequencial.append(palavras[i])
+
+    for k in range(3): # varia os tamanhos M
+        for l in range (4): # varia as funções hash 
             num_experimento += 1
             print("----------------------------------------")
             print(f"Experimento {num_experimento}\n")
             print(f"Texto: {textos[j]}")
             print(f"Função hash: {nomes_funcoes_hash[l]}")
 
-            # criação da lista sequencial e da tabela hash
+            # criação da tabela hash
             tamanho_M = M[k]
-            lista_sequencial = []
             tabela_hash = [] * tamanho_M
             for i in range (tamanho_M):
                 tabela_hash.append([]) # cada item do vetor recebe outro vetor
-
-            # tratamento de texto
-            arquivo_de_texto = textos[j]
-            with open(arquivo_de_texto, "r", encoding = "utf-8") as arquivo:
-                texto = arquivo.read()
-                texto_minusculo = texto.lower()
-                palavras = re.findall(r'\b[a-zA-ZÀ-ÿ]+\b', texto_minusculo)
-
-            for i in range (len(palavras)):
-                if (palavras[i] not in lista_sequencial):
-                    lista_sequencial.append(palavras[i])
 
             # inserindo na tabela
             for palavra in lista_sequencial:
@@ -115,25 +128,24 @@ for j in range (2):
             print(f"Número de palavras distintas: {numero_palavras}")
             print(f"Fator de carga: {fator_carga}")
 
-            buckets = []
             
             if(okterminal == "sim"):
                 print("\nHistograma:")
-                for categoria, lista_na_tabela in enumerate(tabela_hash):
-                    total = len(lista_na_tabela)
-                    buckets.append(total)
+
+            buckets = []
+            for categoria, lista_na_tabela in enumerate(tabela_hash):
+                total = len(lista_na_tabela)
+                buckets.append(total)
+                
+                if(okterminal == "sim"):
                     print(f"Categoria: {categoria} - {total*'*'}")
             
-            # csv para armazenar os dados e criar um histograma no excel
-            if(okcsv == "sim"):
-                nome_arquivo_csv = f'histograma_{num_experimento}.csv'
-                with open(nome_arquivo_csv, 'w', newline='', encoding='utf-8') as csvfile:
-                    writer = csv.writer(csvfile, delimiter=',')
-                    writer.writerow(['Bucket', 'Quantidade'])
-                    for categoria, lista_na_tabela in enumerate(tabela_hash):
-                        total = len(lista_na_tabela)
-                        buckets.append(total)
-                        writer.writerow([categoria, total])
+            if(okimagens == "sim"):
+                plt.bar(range(tamanho_M), buckets, color='royalblue')
+                plt.title(f"Experimento {num_experimento} ({nomes_funcoes_hash[l]})", fontsize=12)
+                plt.xlabel('Índice do bucket', fontsize = 10)
+                plt.ylabel('Quantidade de palavras', fontsize = 10)
+                plt.show()
 
             max_bucket = max(buckets)
             media = statistics.mean(buckets)
@@ -171,12 +183,12 @@ for j in range (2):
             print(f"Tempo total decorrido na busca na tabela hash: {segundos_hash_s} segundos.")
             qtd_encontrada_hash_s = len(palavras_encontradas_hash_s)
             print(f"Quantidade de palavras encontradas na tabela hash: {qtd_encontrada_hash_s}.")
+            avg_comp_s = comparacoes_hash_s/numero_palavras
 
             print(f"\nComparações na estrutura linear: {comparacoes_vetor_s}.")
             print(f"Tempo total decorrido na busca na estrutura linear: {segundos_vetor_s} segundos.")
             qtd_encontrada_vetor_s = len(palavras_encontradas_vetor_s)
             print(f"Quantidade de palavras encontradas na estrutura linear: {qtd_encontrada_vetor_s}.")
-            avg_comp_s = comparacoes_hash_s/numero_palavras
 
             # conjunto falha: arquivo de nomes já filtrado por fora
             comparacoes_hash_f = 0
@@ -185,11 +197,6 @@ for j in range (2):
             segundos_vetor_f = 0
             palavras_encontradas_hash_f = []
             palavras_encontradas_vetor_f = []
-
-            lista_falha = []
-            with open("nomes_filtrados.txt", "r", encoding="utf-8") as arquivo:
-                conjunto_falha = arquivo.read()
-                lista_falha = conjunto_falha.split()
 
             print("----------------------------------------")
             print("Buscando todas as palavras do conjunto falha...")
@@ -210,12 +217,12 @@ for j in range (2):
             print(f"Tempo total decorrido na busca na tabela hash: {segundos_hash_f} segundos.")
             qtd_encontrada_hash_f = len(palavras_encontradas_hash_f)
             print(f"Quantidade de palavras encontradas na tabela hash: {qtd_encontrada_hash_f}.")
-
+            avg_comp_f = comparacoes_hash_f/len(lista_falha)
+            
             print(f"\nComparações na estrutura linear: {comparacoes_vetor_f}.")
             print(f"Tempo total decorrido na busca na estrutura linear: {segundos_vetor_f} segundos.")
             qtd_encontrada_vetor_f = len(palavras_encontradas_vetor_f)
             print(f"Quantidade de palavras encontradas na estrutura linear: {qtd_encontrada_vetor_f}.")
-            avg_comp_f = comparacoes_hash_f/numero_palavras
 
             # armazenando os resultados em um arquivo csv
             with open('resultados.csv', 'a', newline='', encoding='utf-8') as csvfile:
